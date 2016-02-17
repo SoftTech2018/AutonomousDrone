09e2c8d657ab2fb7cf9b3387994f175019029d7e
ministeren <fcmikkelsen@gmail.com>
2016-02-17 12:06:55 +0100

    First commit

---
+++ AutonomousDrone/src/Main.java
@@ -0,0 +1,25 @@
+import de.yadrone.base.ARDrone;
+import de.yadrone.base.IARDrone;
+
+public class Main {
+
+	public static void main(String[] args) {IARDrone drone = null;
+    try
+    {
+        drone = new ARDrone();
+        drone.start();
+    }
+    catch (Exception exc)
+	{
+		exc.printStackTrace();
+	}
+	finally
+	{
+		if (drone != null)
+			drone.stop();
+		System.exit(0);
+	}
+
+	}
+
+}
