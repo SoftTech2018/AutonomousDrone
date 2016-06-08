cf160e8a00a749b4f4a9a78b37677afddfe03b2a
eggman1979 <kim.d.rasmussen@live.dk>
2016-06-08 11:40:52 +0200

    Tilføjet to klasser S2 og M2, som er ment til at vende koordinatsystemet rigtigt.
    Har opdateret wallmarks

---
+++ AutonomousDrone/src/diverse/koordinat/M2.java
@@ -0,0 +1,25 @@
+package diverse.koordinat;
+
+import diverse.circleCalc.Vector2;
+
+public class M2 {
+	public double a,b,
+				  c,d;
+	
+	M2(double a, double b,
+	   double c, double d){
+		this.a = a;this.b=b;
+		this.c = c;this.d=d;
+	}
+	
+	public M2 mul(M2 m){
+		return new M2(a*m.a+b*m.c, a*m.b+b*m.d,
+					  c*m.a+d*m.c, c*m.b+d*m.d);
+	}
+	
+	public Vector2 mul(Vector2 v){
+		return new Vector2(a*v.x+b*v.y,
+					  c*v.x+d*v.y);
+	}
+
+}
