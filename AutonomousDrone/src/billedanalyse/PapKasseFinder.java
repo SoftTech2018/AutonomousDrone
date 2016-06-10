1ed3039ac026fae34e8af09fdb52df5550e90f76
ministeren <fcmikkelsen@gmail.com>
2016-06-10 10:14:54 +0200

    distance beregning

--- AutonomousDrone/src/billedanalyse/PapKasseFinder.java
+++ AutonomousDrone/src/billedanalyse/PapKasseFinder.java
@@ -18,11 +18,11 @@ import org.opencv.imgproc.Moments;
 import boofcv.gui.image.ShowImages;
 import boofcv.io.UtilIO;
 import boofcv.io.image.UtilImageIO;
+import diverse.PunktNavigering;
 
 public class PapKasseFinder {
 
-	private static final double MAX_OBJECT_AREA = 5000;
-	private static final double MIN_OBJECT_AREA = 500;
+	private static final int REAL_HEIGHT = 390;
 
 	/**
 	 * Finder papkasser baseret på farve i et billede
@@ -30,8 +30,10 @@ public class PapKasseFinder {
 	 * @return Koordinaterne for centrum af de identificerede papkasser. Returnerer null hvis intet blev fundet
 	 */
 	public Mat findPapkasse(Mat org){
+		
+		PunktNavigering pn = new PunktNavigering();
 
-		//		org = bufferedImageToMat(UtilImageIO.loadImage(UtilIO.pathExample("C:/Users/ministeren/git/AutonomousDrone/AutonomousDrone/kasse12.jpg")));
+//				org = bufferedImageToMat(UtilImageIO.loadImage(UtilIO.pathExample("C:/Users/ministeren/git/AutonomousDrone/AutonomousDrone/kasse12.jpg")));
 
 
 		Mat out = new Mat();
@@ -98,19 +100,43 @@ public class PapKasseFinder {
 		int sumy = 0;
 		int centerx = 0;
 		int centery = 0;
-		if (!centers.isEmpty()){
-			for(Point p : centers){
-				sumx+=p.x;
-				sumy+=p.y;
+		Point kassecenter = new Point();
+		Point kasseh = new Point();
+		Point kassel = new Point();
+		if (!centers.isEmpty()&& centers.size()>=2){
+//			for(Point p : centers){
+//				sumx+=p.x;
+//				sumy+=p.y;
+//			}
+//			centerx = sumx / centers.size();
+//			centery = sumy / centers.size();
+			for(int i = 0; i<centers.size(); i++){
+				for(int j = i+1; j<centers.size(); j++){
+					if(j<centers.size()){
+						double centerix = centers.get(i).x;
+						double centerjx = centers.get(j).x;
+						if(centerix>centerjx*0.9 && centerix<centerjx*1.1){
+							int ydiff = (int) Math.abs(centers.get(i).y-centers.get(j).y);
+							int ydiffhalv = ydiff/2;
+							double nyy = 0;
+							if(centers.get(i).y<centers.get(j).y){
+								nyy = centers.get(i).y+ydiffhalv;
+							} else {
+								nyy = centers.get(i).y-ydiffhalv;
+							}
+							kassecenter.x = centerix;
+							kassecenter.y = nyy;
+							int dist = (int) pn.calcDist(ydiff, REAL_HEIGHT);
+							Imgproc.circle(org, new Point(kassecenter.x,kassecenter.y), 2, new Scalar(255, 255, 255),2);			
+							Imgproc.putText(org, " "+dist+" mm", new Point(kassecenter.x,kassecenter.y), 1, 2, new Scalar(255, 255, 255), 2);							
+						}
+					}
+				}
 			}
-			centerx = sumx / centers.size();
-			centery = sumy / centers.size();
-			Imgproc.circle(org, new Point(centerx,centery), 2, new Scalar(255, 255, 255),2);			
-			Imgproc.putText(org, "center", new Point(centerx,centery), 1, 2, new Scalar(255, 255, 255), 2);
-		}		
+		}
 
 		//		UtilImageIO.saveImage(toBufferedImage(org), UtilIO.pathExample("C:/Users/ministeren/git/AutonomousDrone/AutonomousDrone/kasse.png"));
-		//		ShowImages.showWindow(toBufferedImage(org),"Title",true);
+//				ShowImages.showWindow(toBufferedImage(org),"Title",true);
 		return out;
 	}
 
