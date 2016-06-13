a427ecebd5c0b084fbef766c9f19a7ba2ff2f6f8
Ebbe <Ebbeb2424@gmail.com>
2016-06-13 09:29:54 +0200

    Tilføjet test metode til at teste om vi finder objekter på gulvet - dog skal der ændres metode i OpgRum så det kan testes

--- AutonomousDrone/src/diverse/GradTest.java
+++ AutonomousDrone/src/diverse/GradTest.java
@@ -1,13 +1,26 @@
 package diverse;
 
+import java.util.ArrayList;
+
+import billedanalyse.ColorTracker;
+import billedanalyse.Squares;
+import diverse.koordinat.Koordinat;
+import diverse.koordinat.OpgaveRum;
 import drone.IDroneControl;
 
 public class GradTest implements Runnable{
 	
 	boolean OPGAVE_DEBUG = false;
 	private IDroneControl dc;
+	private ColorTracker ct;
+	private OpgaveRum opgrum;
 	protected boolean doStop = false;
 	private boolean flying = false;
+	private ArrayList<Koordinat> searchPoints, objectCoords;
+	private ArrayList<Squares> squarePoints;
+	private Koordinat landingsPlads, papKasse, baneStart;
+	long stopTid, startTid;
+	private double mspercm = 10.8;
 
 	
 	public GradTest(IDroneControl dc) {
@@ -16,12 +29,8 @@ public class GradTest implements Runnable{
 	
 	@Override
 	public void run() {
-		try {
-			this.startTest();
-		} catch (InterruptedException e) {
-			this.destroy();
-			return;
-		}		
+		squarePoints = ct.getSquares();
+		this.getSquaresPositioner(squarePoints);		
 	}
 	
 	private void destroy() {
@@ -39,10 +48,36 @@ public class GradTest implements Runnable{
 		return;
 	}
 	
-	public void startTest() throws InterruptedException {
-		dc.strafeLeft(700);
-		Thread.sleep(500);
-		dc.hover();
-	}
+//	public void startTest() throws InterruptedException {
+//		dc.strafeLeft(700);
+//		Thread.sleep(500);
+//		dc.hover();
+//	}
 	
+	/** Modtager et array af squares der gennemløbes og sendes videre til behandling i OpgaveRum,
+	 *  som returnerer et koordinat for hver square der gennemløbes 
+	 *  i square-arrayet. Herefter gennemløbes koordinat-arrayet og tjek for objekternes position udføres 
+	 *  og sendes til OpgaveRum så de kan ses i GUI'en
+	 * @param squares
+	 */
+	private void getSquaresPositioner(ArrayList<Squares> squares) {
+		
+		stopTid = System.currentTimeMillis();
+		Koordinat dronePos = baneStart;
+		
+		// Opdater dronepositionen med tiden og retningen siden dronen sidst opdaterede sin position
+		for(Squares item: squares) {
+			long squaresdif = startTid - item.getTid();
+			int afstand = (int) (squaresdif/mspercm); 
+
+			if (baneStart.getY() < 500) {
+				dronePos.setY(baneStart.getY() + afstand);
+			} else if (baneStart.getY() > 500) {
+				dronePos.setY(baneStart.getY() - afstand);
+			}
+
+			Koordinat objectcoord = opgrum.rotateCoordinate(item, dronePos);
+			objectCoords.add(objectcoord);
+		}	
+	}
 }
